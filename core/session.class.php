99eec24fed1cb79ce8919d3eb8af8582df776713
Vk <v@yopmail.com>
2023-03-03 17:19:17 +0530

    changed the handler

--- core/session.class.php
+++ core/session.class.php
@@ -35,28 +35,28 @@ class Session implements \SessionHandlerInterface {
 	 * memcache object
 	 * @var Memcache $_db
 	 */
-	private static $_db;
+	private $_db;
 
 	/**
 	 *
 	 * keyname of the cipher
 	 * @var string $_keyName
 	 */
-	private static $_keyName;
+	private $_keyName;
 
 	/**
 	 *
 	 * initial value size of the cipher
 	 * @var int $_ivSize
 	 */
-	private static $_ivSize;
+	private $_ivSize;
 
 	/**
 	 *
 	 * key of the cipher
 	 * @var string $_key
 	 */
-	private static $_key ;
+	private $_key ;
 
 	/**
 	 *
@@ -64,18 +64,18 @@ class Session implements \SessionHandlerInterface {
 	 * @param string $save_path
 	 * @param string $session_name
 	 */
-	public static function open($save_path, $session_name) {
-		self::$_db = self::getMemcache();
-		self::$_ivSize = mcrypt_get_iv_size(self::CIPHER, self::CIPHER_MODE);
+	public function open($save_path, $session_name):bool {
+		$this->_db = $this->getMemcache();
+		$this->_ivSize = mcrypt_get_iv_size(Session::CIPHER, Session::CIPHER_MODE);
 		self::$_keyName = 'y'.$session_name;
 
-		if (empty($_COOKIE[self::$_keyName])) {
-			$keyLength = mcrypt_get_key_size(self::CIPHER, self::CIPHER_MODE);
-			self::$_key = self::_randKey($keyLength);
+		if (empty($_COOKIE[$this->_keyName])) {
+			$keyLength = mcrypt_get_key_size(Session::CIPHER, Session::CIPHER_MODE);
+			$this->_key = $this->_randKey($keyLength);
 			$cookie_param = session_get_cookie_params();
 			setcookie(
-			         self::$_keyName,
-			         base64_encode(self::$_key),
+			         $this->_keyName,
+			         base64_encode($this->_key),
 			         $cookie_param['lifetime'],
 			         $cookie_param['path'],
 			         $cookie_param['domain'],
@@ -84,7 +84,7 @@ class Session implements \SessionHandlerInterface {
 			    );
 		}
 		else {
-			self::$_key = base64_decode($_COOKIE[self::$_keyName]);
+			$this->_key = base64_decode($_COOKIE[$this->_keyName]);
 		}
 
 		return true;
@@ -95,7 +95,7 @@ class Session implements \SessionHandlerInterface {
 	 * read the session
 	 * @param string $id the session id
 	 */
-	public static function read($id) {
+	public function read($id) {
 		$sess_id = 'sess_' . $id;
 		self::_setExpire($sess_id);
 		$data = self::$_db->get($sess_id);
@@ -103,12 +103,12 @@ class Session implements \SessionHandlerInterface {
 		if (empty($data)) {
 			return false;
 		}
-		$iv = substr($data, 0, self::$_ivSize);
-		$encrypted = substr($data, self::$_ivSize);
-		$decrypt = mcrypt_decrypt(self::CIPHER, self::$_key, $encrypted,self::CIPHER_MODE, $iv);
+		$iv = substr($data, 0, $this->_ivSize);
+		$encrypted = substr($data, $this->_ivSize);
+		$decrypt = mcrypt_decrypt(Session::CIPHER, $this->_key, $encrypted, Session::CIPHER_MODE, $iv);
 		$d = rtrim($decrypt, "�");
 
-		return $d;
+		return (string)$d;
 	}
 
 	/**
@@ -116,8 +116,8 @@ class Session implements \SessionHandlerInterface {
 	 * close the session
 	 * trigger on the session_write_close
 	 */
-	public static function close() {
-		return self::$_db->close();
+	public function close():bool {
+		return $this->_db->close();
 	}
 
 	/**
@@ -126,24 +126,24 @@ class Session implements \SessionHandlerInterface {
 	 * @param string $id id of the session
 	 * @param string $session_data session data
 	 */
-	public static function write($id, $session_data) {
+	public function write($id, $session_data):bool {
 		$sess_id = 'sess_' . $id;
 
-		if (self::$_db->get($sess_id . '_expire')) {
-			self::$_db->replace($sess_id . '_expire', time(), 0);
+		if ($this->_db->get($sess_id . '_expire')) {
+			$this->_db->replace($sess_id . '_expire', time(), 0);
 		}
 		else {
-			self::$_db->set($sess_id . '_expire', time(), 0);
+			$this->_db->set($sess_id . '_expire', time(), 0);
 		}
-		$iv = mcrypt_create_iv(self::$_ivSize, MCRYPT_RAND);
+		$iv = mcrypt_create_iv($this->_ivSize, MCRYPT_RAND);
 
 		if ($session_data) {
-			$encrypted = mcrypt_encrypt(self::CIPHER, self::$_key,$session_data, self::CIPHER_MODE, $iv);
-			if (self::$_db->get($sess_id)) {
-				self::$_db->replace($sess_id, $iv . $encrypted);
+			$encrypted = mcrypt_encrypt(Session::CIPHER, $this->_key, $session_data, Session::CIPHER_MODE, $iv);
+			if ($this->_db->get($sess_id)) {
+				$this->_db->replace($sess_id, $iv . $encrypted);
 			}
 			else {
-				self::$_db->set($sess_id, $iv . $encrypted);
+				$this->_db->set($sess_id, $iv . $encrypted);
 			}
 
 			return true;
@@ -157,12 +157,12 @@ class Session implements \SessionHandlerInterface {
 	 * destroy the session. trigger on session_destroy
 	 * @param string $id
 	 */
-	public static function destroy($id) {
+	public function destroy($id):bool {
 		$ds = Ds::connect(ds_token);
 		$sessId = "sess_" . $id;
 		$ds->remove(array('snid' => $id));
-		self::$_db->delete($sessId . "_expire");
-		return self::$_db->delete($sessId);
+		$this->_db->delete($sessId . "_expire");
+		return $this->_db->delete($sessId);
 	}
 
 	/**
@@ -170,7 +170,7 @@ class Session implements \SessionHandlerInterface {
 	 * garbage collector. not implemented as that stuff to memcache
 	 * @param int $maxLifeTime
 	 */
-	public static function gc($maxLifeTime) {
+	public function gc($maxLifeTime) {
 		return true;
 	}
 
@@ -179,7 +179,7 @@ class Session implements \SessionHandlerInterface {
 	 * generate a random key for the cipher
 	 * @param int $keyLength
 	 */
-	private static function _randKey($keyLength = 32) {
+	private function _randKey($keyLength = 32) {
 
 		if (function_exists('openssl_random_pseudo_bytes')) {
 			$rnd = openssl_random_pseudo_bytes($keyLength, $strong);
@@ -202,46 +202,46 @@ class Session implements \SessionHandlerInterface {
 	 * set expire the memcache object
 	 * @param string $key
 	 */
-	private static function _setExpire($key) {
+	private function _setExpire($key) {
 		$lifeTime = ini_get("session.gc_maxlifetime");
-		$expire = self::$_db->get($key . "_expire");
+		$expire = $this->_db->get($key . "_expire");
 
 		if ($expire + $lifeTime < time()) {
-			self::$_db->delete($key);
-			self::$_db->delete($key . "_expire");
+			$this->_db->delete($key);
+			$this->_db->delete($key . "_expire");
 		}
 		else {
-			self::$_db->replace($key . "_expire", time());
+			$this->_db->replace($key . "_expire", time());
 		}
 	}
 
-	public static function loadSession($sessId) {
-		$memcache = self::getMemcache();
+	public function loadSession($sessId) {
+		$memcache = $this->getMemcache();
 		$data = $memcache->get('sess_'.$sessId);
 
 		if (empty($data)) {
 			return false;
 		}
-		$iv = substr($data, 0, self::$_ivSize);
-		$encrypted = substr($data, self::$_ivSize);
-		$decrypt = mcrypt_decrypt(self::CIPHER, self::$_key, $encrypted,self::CIPHER_MODE, $iv);
+		$iv = substr($data, 0, $this->_ivSize);
+		$encrypted = substr($data, $this->_ivSize);
+		$decrypt = mcrypt_decrypt(Session::CIPHER, $this->_key, $encrypted, Session::CIPHER_MODE, $iv);
 		$d = rtrim($decrypt, "�");
 		return $d;
 	}
 
-	public static function getSessionByToken($token) {
-		$sessionId = self::mapToken($token);
-		$session = self::loadSession($sessionId);
+	public function getSessionByToken($token) {
+		$sessionId = $this->mapToken($token);
+		$session = $this->loadSession($sessionId);
 		return unserialize($session)  ;
 	}
 
-	public static function mapToken($token) {
-		$memcache = self::getMemcache();
+	public function mapToken($token) {
+		$memcache = $this->getMemcache();
 		$sessionId = $memcache->get($token);
 		return $sessionId ? $sessionId : false;
 	}	
 	
-	public static function getMemcache() {
+	public function getMemcache() {
 		$memcache = new \Memcache();
 		$memcache->addServer(cache_memcache_server,cache_memcache_port,0,2,2,10);
 		return $memcache;
