a103e64a3755e2149732476a6cd8c25951a83c0d
Vk <v@yopmail.com>
2023-03-06 17:06:54 +0530

    modified the session save handler

--- includes/sessionhandler.inc.php
+++ includes/sessionhandler.inc.php
@@ -1,14 +1,43 @@
 <?php
 	//ini_set('session.save_handler', 'memcache');
 	//ini_set('session.save_path',  'tcp://'.cache_memcache_server .':'. cache_memcache_port.'?persistent=1&amp;weight=1&amp;timeout=1&amp;retry_interval=15');
+	// session_set_save_handler(
+	// 	['Core\Session','open'],
+	// 	['Core\Session','close'],
+	// 	['Core\Session','read'],
+	// 	['Core\Session','write'],
+	// 	['Core\Session','destroy'],
+	// 	['Core\Session','gc']
+	// );
+
+	// Set session save handler
 	session_set_save_handler(
-						array('Core\Session','open'),
-						array('Core\Session','close'),
-						array('Core\Session','read'),
-						array('Core\Session','write'),
-						array('Core\Session','destroy'),
-						array('Core\Session','gc')
-						);
+		// Session open callback
+		function ($save_path, $session_name) {
+			return Core\Session::open($save_path, $session_name);
+		},
+		// Session close callback
+		function () {
+			return Core\Session::close();
+		},
+		// Session read callback
+		function ($session_id) {
+			return Core\Session::read($session_id);
+		},
+		// Session write callback
+		function ($session_id, $session_data) {
+			return Core\Session::write($session_id, $session_data);
+		},
+		// Session destroy callback
+		function ($session_id) {
+			return Core\Session::destroy($session_id);
+		},
+		// Session garbage collection callback
+		function ($maxlifetime) {
+			return Core\Session::gc($maxlifetime);
+		}
+	);
+
 	//get the token
 	$token = isset($_REQUEST['token']) ? $_REQUEST['token'] : null;
 	if(!is_null($token)) {
@@ -17,6 +46,7 @@
 			session_id($sessionId);
 		}
 	}
+
 	session_start();
 	/*
 	 * keeps the session id changed every 10 minutes
