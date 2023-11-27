4d337271920eb12eede416707e28cf5c3db1e532
Vk <v@yopmail.com>
2023-11-27 08:53:26 +0530

    added more security foxes

--- includes/bootstrap.inc.php
+++ includes/bootstrap.inc.php
@@ -8,7 +8,12 @@
 	ini_set('session.cookie_httponly', 1);
 	ini_set('session.use_only_cookies', 1);
 	ini_set('session.cookie_secure', 1);
-	
+	header('Header: always edit Set-Cookie (.*) "$1; HTTPOnly"');
+	header('X-XSS-Protection: 1; mode=block');
+	header('X-Content-Type-Options: nosniff');
+	header('X-Frame-Options: SAMEORIGIN');
+	header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+
 	spl_autoload_register(function ($className) {
 		$knownClasses = array(
 			/*
